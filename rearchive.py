fba8f6dc7eebd2f90e2489c0abb70dd989706179
maeda6uiui <63230315+maeda6uiui@users.noreply.github.com>
2023-07-21 22:01:09 +0900

    Add rearchive.py

---
+++ rearchive.py
@@ -0,0 +1,39 @@
+import argparse
+import gzip
+import json
+from pathlib import Path
+from tqdm import tqdm
+
+def main(args):
+    file_list_filepath:str=args.file_list_filepath
+    output_dirname:str=args.output_dirname
+
+    with open(file_list_filepath,"r",encoding="utf-8") as r:
+        file_info_list:list[dict]=json.load(r)
+
+    output_dir=Path(output_dirname)
+    output_dir.mkdir(exist_ok=True,parents=True)
+
+    for file_info in tqdm(file_info_list):
+        input_filepath:str=file_info["filepath"]
+        input_filepath_hash:str=file_info["filepath_hash"]
+
+        extension=Path(input_filepath).suffix[1:]
+        if extension!="txt":
+            raise RuntimeError(f"Unsupported file extension: {extension}")
+
+        with open(input_filepath,"r",encoding="utf-8",errors="replace") as r:
+            lines=r.read().splitlines()
+
+        output_file=output_dir.joinpath(f"{input_filepath_hash}.txt.gz")
+        with gzip.open(str(output_file),"wt",encoding="utf-8") as wt:
+            for line in lines:
+                wt.write(f"{line}\n")
+
+if __name__=="__main__":
+    parser=argparse.ArgumentParser()
+    parser.add_argument("-i","--file-list-filepath",type=str)
+    parser.add_argument("-o","--output-dirname",type=str)
+    args=parser.parse_args()
+    
+    main(args)
