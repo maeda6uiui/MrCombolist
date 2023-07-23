98895608490ff793a1c57328a220296ed1b2bd52
maeda6uiui <63230315+maeda6uiui@users.noreply.github.com>
2023-07-23 15:32:41 +0900

    Add functionality to resume cleanup process

--- cleanup_tsv.py
+++ cleanup_tsv.py
@@ -4,11 +4,16 @@ from tqdm import tqdm
 
 def main(args):
     parse_info_filepath:str=args.parse_info_filepath
+    progress_output_filepath:str=args.progress_output_filepath
+    start_index:int=args.start_index
     
     with open(parse_info_filepath,"r",encoding="utf-8") as r:
         parse_info_list=json.load(r)
 
-    for parse_info in tqdm(parse_info_list):
+    if start_index is not None:
+        parse_info_list=parse_info_list[start_index:]
+
+    for idx,parse_info in enumerate(tqdm(parse_info_list)):
         records_filepath:str=parse_info["records_filepath"]
 
         with open(records_filepath,"r",encoding="utf-8") as r:
@@ -30,9 +35,21 @@ def main(args):
         with open(records_filepath,"w",encoding="utf-8") as w:
             w.writelines(cleanup_lines)
 
+        #Output cleanup progress to file
+        with open(progress_output_filepath,"w",encoding="utf-8") as w:
+            progress=0
+            if start_index is not None:
+                progress=start_index+idx
+            else:
+                progress=idx
+
+            w.write(f"{progress}\n")
+
 if __name__=="__main__":
     parser=argparse.ArgumentParser()
     parser.add_argument("-i","--parse-info-filepath",type=str)
+    parser.add_argument("-p","--progress-output-filepath",type=str,default="cleanup_progress.txt")
+    parser.add_argument("-s","--start-index",type=int)
     args=parser.parse_args()
 
     main(args)
