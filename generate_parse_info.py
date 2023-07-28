ad771991c377bc5c5c8c1c272fddf7f614b74df1
maeda6uiui <63230315+maeda6uiui@users.noreply.github.com>
2023-07-29 08:21:27 +0900

    Remove option to specify output dirname type

--- generate_parse_info.py
+++ generate_parse_info.py
@@ -10,10 +10,6 @@ def get_md5_hash(value:str)->str:
 def main(args):
     schema_detection_results_filepath:str=args.schema_detection_results_filepath
     output_root_dirname:str=args.output_root_dirname
-    output_dirname_type:str=args.output_dirname_type
-
-    if output_dirname_type not in ["parent","stem"]:
-        raise RuntimeError(f"Unsupported output dirname type: {output_dirname_type}")
 
     output_root_dir=Path(output_root_dirname)
 
@@ -24,17 +20,9 @@ def main(args):
     for schema_detection_result in tqdm(schema_detection_results):
         input_filepath=schema_detection_result["filepath"]
         input_file=Path(input_filepath)
-
-        output_dir=None
-        input_dirname_hash=""
-        input_file_stem=""
-        if output_dirname_type=="parent":
-            input_dirname=input_file.parent.name
-            input_dirname_hash=get_md5_hash(input_dirname)
-            output_dir=output_root_dir.joinpath(input_dirname_hash)
-        elif output_dirname_type=="stem":
-            input_file_stem=input_file.name.split(".",1)[0]
-            output_dir=output_root_dir.joinpath(input_file_stem)
+        
+        input_file_stem=input_file.name.split(".",1)[0]
+        output_dir=output_root_dir.joinpath(input_file_stem)
 
         input_filepath_hash=get_md5_hash(input_filepath)
         records_file=output_dir.joinpath(f"{input_filepath_hash}.tsv")
@@ -43,13 +31,10 @@ def main(args):
         parse_info={
             "input_filepath": input_filepath,
             "input_filepath_hash": input_filepath_hash,
+            "input_file_stem": input_file_stem,
             "records_filepath": str(records_file),
             "parse_errors_filepath": str(parse_errors_file)
         }
-        if output_dirname_type=="parent":
-            parse_info["input_dirname_hash"]=input_dirname_hash
-        elif output_dirname_type=="stem":
-            parse_info["input_file_stem"]=input_file_stem
 
         parse_info_list.append(parse_info)
 
@@ -61,7 +46,6 @@ if __name__=="__main__":
     parser=argparse.ArgumentParser()
     parser.add_argument("-i","--schema-detection-results-filepath",type=str)
     parser.add_argument("-o","--output-root-dirname",type=str)
-    parser.add_argument("-t","--output-dirname-type",type=str,default="stem")
     args=parser.parse_args()
 
     main(args)
