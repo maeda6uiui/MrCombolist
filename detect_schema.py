69a031c9f59965c197dc4c06bc72ffea1fdd7f4c
maeda6uiui <63230315+maeda6uiui@users.noreply.github.com>
2024-01-01 18:28:11 +0900

    Update detect_schema.py

--- detect_schema.py
+++ detect_schema.py
@@ -2,121 +2,145 @@ import argparse
 import gzip
 import json
 import re
+import yaml
 from collections import Counter
+from logging import getLogger,config
 from pathlib import Path
-from tqdm import tqdm
+
+def detect_schema(
+    lines:list[str],
+    r_email_head:re.Pattern,
+    r_email_tail:re.Pattern,
+    r_email_middle:re.Pattern,
+    r_email_only:re.Pattern)->dict[str,str]:
+    schema={
+        "placement": "n/a",
+        "delimiter": "n/a"
+    }
+
+    if len(lines)==0:
+        return schema
+    
+    possible_placements=[]
+    possible_delimiters=[]
+    for line in lines:
+        possible_placement=""
+        possible_delimiter=""
+
+        m_head=r_email_head.search(line)
+        m_tail=r_email_tail.search(line)
+        m_middle=r_email_middle.search(line)
+        m_only=r_email_only.search(line)
+
+        if m_head is not None:
+            possible_placement="email:poh"
+            possible_delimiter=line[m_head.end()-1]
+        elif m_tail is not None:
+            possible_placement="poh:email"
+            possible_delimiter=line[m_tail.start()]
+        elif m_middle is not None:
+            possible_placement="unknown:email:unknown"
+            possible_delimiter=line[m_middle.start()]
+        elif m_only is not None:
+            possible_placement="email"
+            possible_delimiter="n/a"
+        else:
+            possible_placement="n/a"
+            possible_delimiter="n/a"
+
+        possible_placements.append(possible_placement)
+        possible_delimiters.append(possible_delimiter)
+
+    #Get the most frequent placement and delimiter
+    ct_placements=Counter(possible_placements)
+    ct_delimiters=Counter(possible_delimiters)
+
+    placement=ct_placements.most_common(1)[0][0]
+    delimiter=ct_delimiters.most_common(1)[0][0]
+
+    schema={
+        "placement": placement,
+        "delimiter": delimiter
+    }
+    return schema
 
 def main(args):
-    file_list_filepath:str=args.file_list_filepath
-    output_dirname:str=args.output_dirname
+    input_dirname:str=args.input_dirname
+    schema_detection_log_dirname:str=args.schema_detection_log_dirname
     delimiter_candidates:str=args.delimiter_candidates
-    start_index:int=args.start_index
     max_num_lines:int=args.max_num_lines
     max_line_length:int=args.max_line_length
+    start_index:int=args.start_index
+    end_index:int=args.end_index
 
-    with open(file_list_filepath,"r",encoding="utf-8") as r:
-        file_info_list:list[dict]=json.load(r)
+    #Set up logger
+    with open("./logging_config.yaml","r",encoding="utf-8") as r:
+        logging_config=yaml.safe_load(r)
+    
+    config.dictConfig(logging_config)
 
-    if start_index is not None:
-        file_info_list=file_info_list[start_index:]
+    logger=getLogger(__name__)
+    logger.debug(args)
 
-    output_dir=Path(output_dirname)
-    output_dir.mkdir(exist_ok=True,parents=True)
+    #Get all gzip files in the input directory
+    input_dir=Path(input_dirname)
+    input_files=list(input_dir.glob("*.txt.gz"))
 
+    logger.info(f"{len(input_files)} files exist in the input directory")
+
+    #Create a directory for schema detection logs
+    schema_detection_log_dir=Path(schema_detection_log_dirname)
+    schema_detection_log_dir.mkdir(exist_ok=True,parents=True)
+
+    #Set up regular expressions
     r_email_head=re.compile(r"^\S+@\S+\.\S+"+f"[{delimiter_candidates}]")
     r_email_tail=re.compile(f"[{delimiter_candidates}]+"+r"\S+@\S+\.\S+$")
     r_email_middle=re.compile(f"[{delimiter_candidates}]+"+r"\S+@\S+\.\S+"+f"[{delimiter_candidates}]")
     r_email_only=re.compile(r"^\S+@\S+\.\S+$")
 
-    for file_info in tqdm(file_info_list):
-        input_filepath:str=file_info["filepath"]
-        input_filepath_hash:str=file_info["filepath_hash"]
+    #Create a subset of the list if either the start or the end index is specified
+    start_index=start_index if start_index is not None else 0
+    end_index=end_index if end_index is not None else len(input_files)
 
-        input_file=Path(input_filepath)
-        extension=input_file.suffix[1:].lower()
+    input_files=input_files[start_index:end_index]
 
-        fp=None
-        if extension=="txt":
-            fp=input_file.open("r",encoding="utf-8",errors="ignore")
-        elif extension=="gz":
-            fp=gzip.open(input_file,"rt",encoding="utf-8",errors="ignore")
-        else:
-            raise RuntimeError(f"Unsupported extension: {extension}")
+    #Detect schema
+    logger.info("Starting schema detection...")
+    for input_file in input_files:
+        logger.info(f"Processing '{input_file.name}'")
 
         lines=[]
-        for idx,line in enumerate(fp):
-            if idx>max_num_lines:
-                break
+        with gzip.open(input_file,"rt",encoding="utf-8") as rt:
+            for idx,line in enumerate(rt):
+                if idx>max_num_lines:
+                    break
 
-            line=line.strip()
-            line=line[:max_line_length]
-            lines.append(line)
+                line=line[:max_line_length].strip()
+                lines.append(line)
 
-        fp.close()
+        schema=detect_schema(lines,r_email_head,r_email_tail,r_email_middle,r_email_only)
 
-        possible_placements=[]
-        possible_delimiters=[]
-        for line in lines:
-            possible_placement=""
-            possible_delimiter=""
-
-            m_head=r_email_head.search(line)
-            m_tail=r_email_tail.search(line)
-            m_middle=r_email_middle.search(line)
-            m_only=r_email_only.search(line)
-
-            if m_head is not None:
-                possible_placement="email:poh"
-                possible_delimiter=line[m_head.end()-1]
-            elif m_tail is not None:
-                possible_placement="poh:email"
-                possible_delimiter=line[m_tail.start()]
-            elif m_middle is not None:
-                possible_placement="unknown:email:unknown"
-                possible_delimiter=line[m_middle.start()]
-            elif m_only is not None:
-                possible_placement="email"
-                possible_delimiter="n/a"
-            else:
-                possible_placement="n/a"
-                possible_delimiter="n/a"
-
-            possible_placements.append(possible_placement)
-            possible_delimiters.append(possible_delimiter)
-        
-        placement=""
-        delimiter=""
-        if len(possible_placements)>0 or len(possible_delimiters)>0:
-            c_placements=Counter(possible_placements)
-            c_delimiters=Counter(possible_delimiters)
-
-            placement=c_placements.most_common(1)[0][0]
-            delimiter=c_delimiters.most_common(1)[0][0]
-        else:
-            placement="n/a"
-            delimiter="n/a"
-
-        schema={
-            "placement": placement,
-            "delimiter": delimiter
-        }
-        detection_result={
-            "filepath": str(input_file),
+        #Output schema detection result to a log file
+        schema_detection_result={
+            "filename": input_file.name,
             "schema": schema
         }
-        
-        output_file=output_dir.joinpath(f"{input_filepath_hash}.json")
-        with output_file.open("w",encoding="utf-8") as w:
-            json.dump(detection_result,w,ensure_ascii=False,indent=4)
+
+        schema_detection_log_file=schema_detection_log_dir.joinpath(f"{input_file.stem}.json")
+        with schema_detection_log_file.open("w",encoding="utf-8") as w:
+            json.dump(schema_detection_result,w,ensure_ascii=False)
+
+    logger.info("Finished schema detection")
 
 if __name__=="__main__":
     parser=argparse.ArgumentParser()
-    parser.add_argument("-i","--file-list-filepath",type=str)
-    parser.add_argument("-o","--output-dirname",type=str)
+    parser.add_argument("-i","--input-dirname",type=str)
+    parser.add_argument("-l","--schema-detection-log-dirname",type=str)
     parser.add_argument("-d","--delimiter-candidates",type=str,default=":;|, \t")
-    parser.add_argument("-s","--start-index",type=int)
-    parser.add_argument("-n","--max-num-lines",type=int,default=1000000)
-    parser.add_argument("-l","--max-line-length",type=int,default=200)
+    parser.add_argument("--max-num-lines",type=int,default=1000000)
+    parser.add_argument("--max-line-length",type=int,default=200)
+    parser.add_argument("--start-index",type=int)
+    parser.add_argument("--end-index",type=int)
     args=parser.parse_args()
 
     main(args)
