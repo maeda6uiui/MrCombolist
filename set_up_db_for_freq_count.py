f7b83c3fadaa1d44210700fed3cce8f161c73a92
maeda6uiui <63230315+maeda6uiui@users.noreply.github.com>
2024-01-04 12:30:11 +0900

    Add option to remove DB if already exists

--- set_up_db_for_freq_count.py
+++ set_up_db_for_freq_count.py
@@ -2,11 +2,13 @@ import argparse
 import sqlite3
 import yaml
 from logging import getLogger,config
+from pathlib import Path
 
 def main(args):
     db_filepath:str=args.db_filepath
     word_list_filepath:str=args.word_list_filepath
     num_records_to_commit:int=args.num_records_to_commit
+    remove_db_if_exists:bool=args.remove_db_if_exists
 
     #Set up logger
     with open("./logging_config.yaml","r",encoding="utf-8") as r:
@@ -16,6 +18,12 @@ def main(args):
 
     logger=getLogger(__name__)
     logger.debug(args)
+
+    #Remove DB if it already exists
+    db_file=Path(db_filepath)
+    if remove_db_if_exists and db_file.exists():
+        logger.info(f"Remove DB '{db_file.name}'")
+        db_file.unlink()
 
     #Set up DB
     logger.info("Start setting up DB...")
@@ -51,6 +59,7 @@ if __name__=="__main__":
     parser.add_argument("-o","--db-filepath",type=str)
     parser.add_argument("-l","--word-list-filepath",type=str)
     parser.add_argument("-n","--num-records-to-commit",type=int,default=1000000)
+    parser.add_argument("--remove-db-if-exists",action="store_true")
     args=parser.parse_args()
 
     main(args)
