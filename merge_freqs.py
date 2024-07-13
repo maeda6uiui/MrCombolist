880532f389421b56dd330d5192ebf36d16e8684d
maeda6uiui <63230315+maeda6uiui@users.noreply.github.com>
2024-07-13 13:09:15 +0900

    Separate processing stages into gather and merge

--- merge_freqs.py
+++ merge_freqs.py
@@ -4,49 +4,20 @@ import yaml
 from logging import getLogger,config
 from pathlib import Path
 
-def main(args):
-    input_dirname:str=args.input_dirname
-    input_filepaths:str=args.input_filepaths
-    output_filepath:str=args.output_filepath
-    log_filepath:str=args.log_filepath
-    remove_db_if_exists:bool=args.remove_db_if_exists
-    
-    #Set up logger
-    with open("./logging_config.yaml","r",encoding="utf-8") as r:
-        logging_config=yaml.safe_load(r)
-    
-    config.dictConfig(logging_config)
+#Set up logger
+with open("./logging_config.yaml","r",encoding="utf-8") as r:
+    logging_config=yaml.safe_load(r)
 
-    logger=getLogger(__name__)
-    logger.debug(args)
+config.dictConfig(logging_config)
+logger=getLogger(__name__)
 
+def fn_gather_local(input_dirname:str,output_filepath:str):
     #Get input files
-    input_files:list[Path]=[]
-    if input_filepaths is not None:
-        lst_input_filepaths=input_filepaths.split(",")
-
-        for input_filepath in lst_input_filepaths:
-            input_file=Path(input_filepath)
-            if not input_file.exists():
-                logger.error(f"'{str(input_file)}' does not exist")
-                return
-            
-            input_files.append(input_file)
-
-        logger.info(f"{len(input_files)} files are specified as input")
-    else:
-        input_dir=Path(input_dirname)
-        input_files=list(input_dir.glob("*.db"))
-
-        logger.info(f"{len(input_files)} files exist in the input directory")
-
+    input_dir=Path(input_dirname)
+    input_files=list(input_dir.glob("*.db"))
     input_files.sort()
 
-    #Remove DB if already exists
-    output_file=Path(output_filepath)
-    if remove_db_if_exists and output_file.exists():
-        output_file.unlink()
-        logger.info(f"DB file '{output_file.name}' was removed")
+    logger.info(f"{len(input_files)} files exist in the input directory")
 
     #Create table to gather local frequencies
     logger.info("Creating table to gather local frequencies...")
@@ -90,6 +61,7 @@ def main(args):
 
     logger.info("Finished gathering records from local frequency tables")
 
+def fn_merge_local(output_filepath:str):
     #Merge local frequencies
     logger.info("Start merging local frequencies...")
     with sqlite3.connect(output_filepath) as conn:
@@ -124,20 +96,25 @@ def main(args):
 
     logger.info("Finished merging local frequencies")
 
-    #Output list of input files to a log file
-    if log_filepath is not None:
-        log_file=Path(log_filepath)
-        with log_file.open("w",encoding="utf-8") as w:
-            for input_file in input_files:
-                w.write(f"{str(input_file)}\n")
+def main(args):
+    input_dirname:str=args.input_dirname
+    output_filepath:str=args.output_filepath
+    gather_local:bool=args.gather_local
+    merge_local:bool=args.merge_local
+
+    logger.debug(args)
+
+    if gather_local:
+        fn_gather_local(input_dirname,output_filepath)
+    if merge_local:
+        fn_merge_local(output_filepath)
 
 if __name__=="__main__":
     parser=argparse.ArgumentParser()
     parser.add_argument("-i","--input-dirname",type=str)
-    parser.add_argument("-f","--input-filepaths",type=str)
     parser.add_argument("-o","--output-filepath",type=str)
-    parser.add_argument("-l","--log-filepath",type=str)
-    parser.add_argument("--remove-db-if-exists",action="store_true")
+    parser.add_argument("--gather-local",action="store_true")
+    parser.add_argument("--merge-local",action="store_true")
     args=parser.parse_args()
 
     main(args)
