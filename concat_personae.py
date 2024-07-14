0885fe0000a661cf2400aedb5c5f09a332d7d048
maeda6uiui <63230315+maeda6uiui@users.noreply.github.com>
2024-07-14 11:06:07 +0900

    Remove options

--- concat_personae.py
+++ concat_personae.py
@@ -6,10 +6,7 @@ from pathlib import Path
 
 def main(args):
     input_dirname:str=args.input_dirname
-    input_filepaths:str=args.input_filepaths
     output_filepath:str=args.output_filepath
-    log_filepath:str=args.log_filepath
-    remove_db_if_exists:bool=args.remove_db_if_exists
 
     #Set up logger
     with open("./logging_config.yaml","r",encoding="utf-8") as r:
@@ -21,32 +18,11 @@ def main(args):
     logger.debug(args)
 
     #Get input files
-    input_files:list[Path]=[]
-    if input_filepaths is not None:
-        lst_input_filepaths=input_filepaths.split(",")
-
-        for input_filepath in lst_input_filepaths:
-            input_file=Path(input_filepath)
-            if not input_file.exists():
-                logger.error(f"'{str(input_file)}' does not exist")
-                return
-            
-            input_files.append(input_file)
-
-        logger.info(f"{len(input_files)} files are specified as input")
-    else:
-        input_dir=Path(input_dirname)
-        input_files=list(input_dir.glob("*.db"))
-
-        logger.info(f"{len(input_files)} files exist in the input directory")
-
+    input_dir=Path(input_dirname)
+    input_files=list(input_dir.glob("*.db"))
     input_files.sort()
 
-    #Remove DB if already exists
-    output_file=Path(output_filepath)
-    if remove_db_if_exists and output_file.exists():
-        output_file.unlink()
-        logger.info(f"DB file '{output_file.name}' was removed")
+    logger.info(f"{len(input_files)} files exist in the input directory")
 
     #Concatenate personae
     logger.info("Start concatenating personae...")
@@ -88,20 +64,10 @@ def main(args):
 
     logger.info("Finished concatenating personae")
 
-    #Output list of input files to a log file
-    if log_filepath is not None:
-        log_file=Path(log_filepath)
-        with log_file.open("w",encoding="utf-8") as w:
-            for input_file in input_files:
-                w.write(f"{str(input_file)}\n")
-
 if __name__=="__main__":
     parser=argparse.ArgumentParser()
     parser.add_argument("-i","--input-dirname",type=str)
-    parser.add_argument("-f","--input-filepaths",type=str)
     parser.add_argument("-o","--output-filepath",type=str)
-    parser.add_argument("-l","--log-filepath",type=str)
-    parser.add_argument("--remove-db-if-exists",action="store_true")
     args=parser.parse_args()
 
     main(args)
