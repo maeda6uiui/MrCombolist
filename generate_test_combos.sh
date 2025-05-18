c391b230b2e5bc2eedb4160c20a22d3e75760270
maeda6uiui <63230315+maeda6uiui@users.noreply.github.com>
2025-05-18 19:53:25 +0900

    Create multiple combo sets for testing

--- generate_test_combos.sh
+++ generate_test_combos.sh
@@ -1,28 +1,37 @@
 #!/bin/bash
 
+NUM_COMBO_SETS=$1
+
 #Activate venv
 source venv/bin/activate
 
 #Generate small pseudo combos for testing
-python main.py generate-pseudo-combos \
-    -nw 10000 \
-    -e 0.example.com,1.example.com,2.example.com \
-    -d ":" \
-    -nc 100000 \
-    -o ./tests/Data/delim_colon_100000.txt
-python main.py generate-pseudo-combos \
-    -nw 10000 \
-    -e 0.example.com,1.example.com,2.example.com \
-    -d "|" \
-    -nc 100000 \
-    -o ./tests/Data/delim_pipe_100000.txt
+upper_bound=$((${NUM_COMBO_SETS}-1))
+for i in $(seq 0 ${upper_bound})
+do
+    python main.py generate-pseudo-combos \
+        -nw 10000 \
+        -e 0.example.com,1.example.com,2.example.com \
+        -d ":" \
+        -nc 100000 \
+        -o ./tests/Data/delim_colon_100000.txt
+    python main.py generate-pseudo-combos \
+        -nw 10000 \
+        -e 0.example.com,1.example.com,2.example.com \
+        -d "|" \
+        -nc 100000 \
+        -o ./tests/Data/delim_pipe_100000.txt
 
-#Create a tarball of the pseudo combos
-cd ./tests/Data
-tar -zcf \
-    test_combos.tar.gz \
-    delim_colon_100000.txt \
-    delim_pipe_100000.txt
+    #Create a tarball of the pseudo combos
+    cd ./tests/Data
+    tar -zcf \
+        test_combos_${i}.tar.gz \
+        delim_colon_100000.txt \
+        delim_pipe_100000.txt
 
-#Remove generated text files
-rm delim_colon_100000.txt delim_pipe_100000.txt
+    #Remove generated text files
+    rm delim_colon_100000.txt delim_pipe_100000.txt
+
+    #Move to original directory
+    cd ../..
+done
